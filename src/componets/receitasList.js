da50761ce335d10d9b88e5a9ef5d1848a2501754
teceudesenvolvo <tecnologia@eudesenvolvo.com>
2023-02-25 18:43:18 -0300

    receitas-vacinas

---
+++ src/componets/receitasList.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+
+//Imagen
+
+// Icones
+
+
+// Components
+
+//mudança de páginas
+
+class receitaList extends Component {
+    state = {
+        receitas: [
+            {
+                id: '1',
+                title: 'Receita Médica',
+                date: '20/02/2023',
+                protocolo: '10129819378234',
+                remedios: [
+                    {
+                        id: '1',
+                        nome: 'Tilenol',
+                        uso: 'Oral',
+                        intervalo: '8 horas',
+                        dose: '1 comprimido',
+                    },
+                    {
+                        id: '2',
+                        nome: 'Dipirona',
+                        uso: 'Oral',
+                        intervalo: '12 horas',
+                        dose: '2 comprimidos',
+                    }
+                ]
+            },
+
+        ]
+    }
+
+
+
+
+    render() {
+        const receitas = this.state.receitas
+
+        const listCategories = receitas.map((receita) =>
+            <li key={(receita.id)} className="favoritoItem vacinaItem"
+                onClick={
+                    () => {
+                        window.location.href = "/produto"
+                        // this.setState({id: aviso.id}, () => {
+                        // (this.props.clickButton(this.state))
+                        //   }
+                    }
+                }
+            >
+                <div className='areaTextDescProductF vacinaItem' >
+                    <p className='valueProduct' >{receita.title}</p>
+                    <p className='descricaoProduct' >{receita.date}</p>
+                    <p className='descricaoProduct' > Protocolo: {receita.protocolo}</p>
+
+                    <p className='valueProduct' >Prescrição</p>
+                    <ul>
+                        
+                    </ul>
+
+                </div>
+            </li>
+        )
+
+
+        return (
+            <>
+                <ul className='vistosHome'>
+                    {listCategories}
+                </ul>
+            </>
+
+        );
+    }
+}
+
+export default receitaList;
