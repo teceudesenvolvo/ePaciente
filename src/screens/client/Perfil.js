7ce57893aa3957c0f37f742a1e81e2207a8928c9
teceudesenvolvo <tecnologia@eudesenvolvo.com>
2023-02-28 20:03:36 -0300

    profilePage

--- src/screens/client/Perfil.js
+++ src/screens/client/Perfil.js
@@ -7,6 +7,7 @@ import React, { Component } from 'react';
 // Icones
 
 // Components
+import TopBar from '../../componets/topBarSearch'
 
 
 //mudança de páginas
@@ -16,18 +17,30 @@ class Perfil extends Component {
         return (
 
             <div className='App-header' >
-                <p>Perfil</p>
-                <form className='formPerfil'>
-                    <input type="text" placeholder="Leonardo" className='inputPerfil' />
-                    <input type="text" placeholder="076.***.***-**" className='inputPerfil' />
-                    <input type="text" placeholder="nome@email.com" className='inputPerfil' />
-                    <input type="text" placeholder="(85)99999-9999" className='inputPerfil' />
-                    <input type="text" placeholder="Rua Fulano de Tal - 55" className='inputPerfil' />
-                    <input type="text" placeholder="Master Card - Crédito **** **** **** 9898" className='inputPerfil' />
-                   
-                    
-                </form>
+                <div className='header-home'>
+                    <TopBar />
+                </div>
+                <div className='favoritos agendarConsulta'>
+                    <h1>Informações Pessoais</h1>
+
+
+                    <ul className='vistosHome'>
+                        <li className="favoritoItem vacinaItem">
+                            <div className='areaTextDescProduct' >
+                                <p className='descricaoProduct' ><b>Nome Completo</b></p>
+                                <p className='descricaoProduct authorProduct' >Nascimento: 03/11/1997</p>
+                                <p className='descricaoProduct' >Sexo: <b>Masculino</b></p>
+                                <p className='descricaoProduct' >Tipo Sanguineo: <b>O-</b></p>
+                                <p className='descricaoProduct' >CPF:</p>
+                                <p className='descricaoProduct' >RG:</p>
+                                <p className='descricaoProduct' >CNS<b>123 1234 1234 1234</b></p>
+                                <p className='descricaoProduct' >Endereço</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
+
         );
     }
 }
